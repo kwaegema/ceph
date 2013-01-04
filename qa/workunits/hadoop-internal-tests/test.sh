059a9117800f12dba73f750b9b1fa7d39b05c3ab
Joe Buck <jbbuck@gmail.com>
2013-01-03 22:05:07 -0800

    Trying to get this working.
    Signed-off-by: Joe Buck <jbbuck@gmail.com>

--- qa/workunits/hadoop-internal-tests/test.sh
+++ qa/workunits/hadoop-internal-tests/test.sh
@@ -7,13 +7,17 @@ echo "starting hadoop-internal-tests tests"
 
 export LD_LIBRARY_PATH=$BASE/$TLIB 
 #command="java -DCEPH_CONF_FILE=$BASE/ceph.conf -Djava.library.path=$LD_LIBRARY_PATH -cp /usr/share/java/junit4.jar:$BASE/$TLIB/libcephfs.jar:$BASE/$TLIB/libcephfs-test.jar org.junit.runner.JUnitCore com.ceph.fs.CephAllTests"
-command="cd $BASE/hadoop; ant -Dextra.library.path=$BASE/$TLIB -Dceph.conf.file=$BASE/ceph.conf test -Dtestcase=TestCephFileSystem"
+command1="cd $BASE/hadoop" 
+command2="ant -Dextra.library.path=$BASE/$TLIB -Dceph.conf.file=$BASE/ceph.conf test -Dtestcase=TestCephFileSystem"
 
 echo "----------------------"
-echo $command
+echo $command1
+echo "----------------------"
+echo $command2
 echo "----------------------"
 
-$command
+$command1
+$command2
 
 echo "completed hadoop-internal-tests tests"
 
